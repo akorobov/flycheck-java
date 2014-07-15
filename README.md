c4e4480a98c716b91ae9e4842011a2c0b4a661c6
Alexander Korobov <akorobov@gmail.com>
2014-07-15 15:37:45 -0700

    Clean up/rework checker code

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
+
 flycheck-java
 =============
+### Description
 
-flycheck-java is on-the-fly java syntax checker that supports both flycheck and flymake.  
-* Supports standard or custom java project directory structure  
-* Automatically picks up eclipse project definition (if available)  
-* Uses eclipse batch compiler for syntax checking (invoked directly of via drip)  
-* Uses drip (https://github.com/flatland/drip) if available
+flycheck-java is on-the-fly java syntax [flycheck](https://github.com/flycheck/flycheck) checker using Eclipse's [batch compiler](http://help.eclipse.org/juno/index.jsp?topic=%2Forg.eclipse.jdt.doc.user%2Ftasks%2Ftask-using_batch_compiler.htm).
+
+flycheck-java can figure out project structure/layout from Eclipse project files(.project,.classpath), otherwise it assumes [standard java project structure](http://maven.apache.org/guides/introduction/introduction-to-the-standard-directory-layout.html).
+
+This checker is meant to be used as auxiliary tool together with external build system (ide, mvm, ant, etc).
+
+### Customization 
+
+`flycheck-java-ecj-jar-path` option has to configured to point to ECJ jar file, you can customize it using `M-x customize-variable' or programmatically:
+
+```
+(add-hook 'java-mode-hook
+          (lambda () (setq flycheck-java-ecj-jar-path "/path/to/ecj-4.2.jar")))
+```
+
 
--- flycheck-java.el
+++ flycheck-java.el
@@ -1,86 +1,63 @@
-;; Author: akorobov@gmail.com
+;;; flycheck-java --- On the fly java syntax checking  -*- lexical-binding: t; -*-
 
-;; Provides on-the-fly java syntax checking using flycheck or flymake.
-;; Supports standard or custom java project directory structure
-;; Automatially picks up eclipse project definition (if available)
-;; Uses eclipse batch compiler for syntax checking (invoked directly of via drip)
-;; Uses drip (https://github.com/flatland/drip) if available
+;;; Commentary:
+;; Copyright (c) 2013,2014 Alexander Korobov 
+;; Author: https://github.com/akorobov
+;; URL: https://github.com/akorobov/flycheck-java
+;; Version: 0.2
+;; Package-Requires: ((s "1.9.0") (dash "2.4.0")  (pkg-info "0.4") (cl-lib "0.3") (emacs "24.3") (flycheck "0.17"))
+;; License: BSD
 
-;; Can be used with both flycheck and flymake
-;; Requires flycheck version 0.10 (83aac8b326) or later
+;; Provides support for on the fly java syntax checking using flycheck and eclipse
+;; batch compiler. Expects java project to use standard project directory layout/structure, able to extract project layout from eclipse project files.
+
+;;; Code:
 
 (require 'xml)
+(require 's)
+(require 'dash)
 
-(defconst jsc/eclipse-project-file-name ".project")
-(defconst jsc/lisp-project-file-name ".project.el")
+(defvar-local flycheck-java-project nil
+  "Variable containing java project definition for current file")
+
+(defvar-local  flycheck-java-cmdopts nil
+  "Variable containing ecj command line options")
+
+(defconst flycheck-java-eclipse-project-file-name ".project")
+(defconst flycheck-java-lisp-project-file-name ".project.el")
 
 ;; by default java compiler tries to build transitive closure of source code
 ;; (all referenced classes) so in large codebases running compiler and parsing
 ;; errors takes way too much time/resources. no-sourcepath variable controls
 ;; inclusion of source directories into compiler command. when it is set
 ;; compiler is given only current classpath (where compiled classes reside).
-;; This speeds up compilation/syntax checking at expense of running on possibly
+;; This speeds up compilation/syntax checking at expense of runing on possibly
 ;; stale data.
-(defvar jsc/no-sourcepath t)
+(defvar flycheck-java-no-sourcepath t)
 
-(defvar jsc/ecj-jar-path
-  (expand-file-name  "~/dev/tools/ecj-4.2.jar")
-  "Location of ecj batch compiler jar")
+(flycheck-def-option-var flycheck-java-ecj-jar-path (expand-file-name  "~/dev/tools/ecj-4.2.jar") java
+  "A path to ECJ jar file."
+  :type 'string
+  :package-version '(flycheck . "0.17")
+  :safe #'stringp)
 
-(defconst jsc/default-java-project
-  '((:source . "1.6")
-    (:target . "1.6")
-    (:options . "-warn:+over-ann,uselessTypeCheck -proceedOnError -maxProblems 100")
-    ;; source code -> class path mappings
-    (:paths . (("src/main/java" . "target/classes")
-              ("src/test/java" . "target/test-classes")))
+;; Standard java project definition
+(defconst flycheck-java-standard-java-project-def
+  '(:type :standard
+    :source "1.6"
+    :target "1.6"
+    :options "-warn:+over-ann,uselessTypeCheck -proceedOnError -maxProblems 100"
+
+    ;; source directory -> class directory mappings
+    :paths (("src/main/java" . "target/classes") 
+            ("src/test/java" . "target/test-classes"))
+
     ;; library paths to check
-    (:lib-paths . ("lib" "target/dependency")))
-    "Standard java project definition")
+    :lib-paths ("lib" "target/dependency"))
+    "Standard java project layout and definition.")
 
-(defvar jsc/java
-  (let ((drip (executable-find "drip")))
-    (if drip
-        drip
-      "java"))
-    "Use drip if available, otherwise java")
-
-(defun jsc/save-project ()
-  (interactive)
-  "Saves project definition in file "
-  (when (local-variable-p 'current-java-project)
-    (let ((proj-file-name (jsc/write-project-def jsc/lisp-project-file-name current-java-project)))
-      ((match-end N)ssage "Saved project definition to %S" proj-file-name))))
-
-(defun jsc/reset
-  (interactive)
-  "Clears current java syntax checker state and causes project definition to be re-read during next check"
-  (kill-local-variable 'current-java-project)
-  (kill-local-variable 'compiler-cmd-options))
-
-(defun jsc/read-project ()
-  (interactive)
-  "Re-reads java project definition for current buffer"
-  (kill-local-variable 'current-java-project)
-  (jsc/get-ecj-compiler-options))
-
-(defun jsc/write-project-def (fname project)
-  (let ((proj-file-name (concat (cdr (assoc ':project-dir project)) fname)))
-    (with-temp-file proj-file-name
-      (insert (let ((print-length nil))
-                (pp-to-string project)))
-      proj-file-name)))
-
-(defun jsc/read-project-def (dir)
-  (let ((root-dir (locate-dominating-file dir jsc/lisp-project-file-name)))
-    (if root-dir
-        (read (with-temp-buffer
-                (insert-file-contents (concat root-dir jsc/lisp-project-file-name))
-                (buffer-string)))
-      nil)))
-
-(defun jsc/get-abs-path (p e) (expand-file-name (concat p "/" e)))
-(defun jsc/parse-eclipse-classpath (cp-file)
+(defun flycheck-java-parse-eclipse-classpath (cp-file)
+  "Parse Eclipse's .classpath CP-FILE file to extract source/class path mappings and libraries."
   (if cp-file
       (let* ((classpath-entries (xml-get-children (car (xml-parse-file cp-file)) 'classpathentry))
              (paths ())
@@ -94,145 +71,110 @@
                         (add-to-list 'paths (cons path output)))
                         ((string= "lib" kind)
                          (add-to-list 'libs path))))) classpath-entries)
-        (list (cons ':paths paths) (cons 'libs libs)))
-      ))
+        (list :paths paths :libs libs))))
 
-(defun jsc/read-eclipse-project (dir)
-  (let ((ecp (jsc/parse-eclipse-classpath (concat dir  "/.classpath"))))
-    ;; create new project assoc list
-    (list (cons ':project-dir dir)
-          (assoc :source jsc/default-java-project)
-          (assoc ':target jsc/default-java-project)
-          (assoc ':options jsc/default-java-project)
-          (assoc ':paths ecp)
-          (assoc 'libs ecp))))
+(defun flycheck-java-read-eclipse-project (dir)
+  "Parse Eclipse's project file(s) located in DIR directory and return java project definition."
+  (let ((ecp (flycheck-java-parse-eclipse-classpath (concat dir  "/.classpath"))))
 
-;; custom error parser is required to parse error/warning column
-(defun jsc/ecj-compile-pattern (s level)
-  (list (concat "^\\(?1:.*\\):\\(?2:[0-9]+\\): " s ": +\\(?4:.*\\)$") level))
+    ;; return project definition
+    (append  (list :project-dir dir
+                   :source (plist-get flycheck-java-standard-java-project-def :source)
+                   :target (plist-get flycheck-java-standard-java-project-def :target)
+                   :options (plist-get flycheck-java-standard-java-project-def :options)
+                   ) ecp)))
 
+(defun flycheck-java-match-project-def (dir project-def)
+  "Try to match source directory DIR to java project template PROJECT-DEF.  If successful return project type and root directory, otherwise return nil."
+  (let* ((paths (-map 'car (plist-get project-def :paths)))
+         (pattern (s-concat "\\(.*\\)\\("
+                            (--reduce (s-concat acc "\\|" it) paths)
+                            "\\)\\(.*\\)"))
+         (match (s-match pattern dir)))
+    (if match
+        (cons (plist-get project-def :type) (nth 1 match))))
+  )
 ;; returns tuple of type and root directory of the project
-(defun jsc/find-project (dir)
-  (let ((eclipse-dir (locate-dominating-file dir jsc/eclipse-project-file-name))
-        (fmj-dir (locate-dominating-file dir jsc/lisp-project-file-name)))
-    (if fmj-dir (cons 'lisp fmj-dir)
-      (if eclipse-dir (cons 'eclipse eclipse-dir)
-        (let* ((paths (cdr (assoc ':paths jsc/default-java-project)))
-               ;; match paths that contain either of project source paths
-               (pat (concat "\\(.*\\)\\(" (mapconcat #'car paths "\\|") "\\)\\(.*\\)")))
-          (if (string-match pat dir)
-              (cons 'standard (match-string 1 dir))
-            '(unknown . unknown)))
-         ))))
+(defun flycheck-java-find-project (dir)
+  ""
+  (let ((eclipse-dir (locate-dominating-file dir flycheck-java-eclipse-project-file-name))
+        (fmj-dir (locate-dominating-file dir flycheck-java-lisp-project-file-name)))
+    (cond
+     ((stringp fmj-dir) (cons :custom fmj-dir))
+     ((stringp eclipse-dir) (cons :eclipse eclipse-dir))
+     (t (flycheck-java-match-project-def dir flycheck-java-standard-java-project-def)))))
+    
+(defun flycheck-java-get-project-def (dir)
+  "Look up project type and root directory for given source path DIR, if successful read, parse and return project definition."
+  (let* ((project (flycheck-java-find-project dir))
+         (root-dir (cdr project)))
+    (pcase (car project)
+      (:eclipse (flycheck-java-read-eclipse-project root-dir))
+      (:custom
+       (cons (cons 'project-dir root-dir)
+             (flycheck-java-read-project-def root-dir)))
+      (:standard (append `(:project-dir ,root-dir) flycheck-java-standard-java-project-def))
+      (:else
+       (error "unable to determine project root and type")))))
 
-(defun jsc/initialize (dir)
-  ;; delete defaults
-  (let* ((project-info (jsc/find-project dir))
-         (root-dir (cdr project-info)))
+(defun flycheck-java-expand-lib-path (project-dir lib-path)
+  "Given PROJECT-DIR and LIB-PATH directory containing jar/zip files produce list lib-path/lib1.jar ... lib-path/libN.jar of libraries."
+  (let ((abs-dir (expand-file-name lib-path project-dir)))
+    (when (file-exists-p abs-dir)
+      (--map (concat lib-path "/" it)
+             (--filter (not (s-contains? "-sources" it))
+                       (directory-files abs-dir nil ".*\\(jar\\|zip\\)"))))))
 
-    (case (car project-info)
-      (eclipse (jsc/read-eclipse-project root-dir))
-      (lisp
-       (cons (cons ':project-dir root-dir)
-             (jsc/read-project-def root-dir)))
-      (standard (cons
-                 (cons ':project-dir root-dir)
-                 (copy-alist jsc/default-java-project)))
-      (else
-       ;; try to guess other common cases
-       (error "unable to find root of project")))))
+(defun flycheck-java-expand-lib-paths (project-dir lib-paths)
+  "Given PROJECT-DIR project root directory and list of relative LIB-PATHS directories produce list of (lib-path1/lib1.jar ... lib-pathN/libM.jar) libraries."
+  (when lib-paths
+    (--mapcat (flycheck-java-expand-lib-path project-dir it) lib-paths)))
 
-
-(defun jsc/expand-lib-path (d)
-  (let ((absd (expand-file-name d project-dir)))
-    (when (file-exists-p absd)
-      (mapcar (lambda (e) (concat d "/" e))
-              (directory-files absd nil ".*\\(jar\\|zip\\)")))))
-
-(defun jsc/expand-lib-paths (project-dir p)
-  (if (assoc 'libs p)
-      (cdr (assoc 'libs p))
-    (let* ((lib-paths (cdr (assoc ':lib-paths p)))
-           (libs (apply #'append (mapcar #'jsc/expand-lib-path lib-paths))))
-      libs)))
-
-(defun jsc/make-compiler-options (p)
-  (let* ((root (cdr (assoc ':project-dir p)))
-         (paths (cdr (assoc ':paths p)))
-         (libs (jsc/expand-lib-paths root p))
-         (source-path (mapconcat (lambda (e)
-                                   (jsc/get-abs-path root (car e))) paths ":"))
-         (class-path (mapconcat (lambda (e)
-                                  (jsc/get-abs-path root e))
-                                (append (mapcar #'cdr paths) libs) ":")))
+(defun flycheck-java-make-cmd-options (p)
+  "Create list of command line options for given project definition P."
+  (let* ((root (expand-file-name (plist-get p :project-dir)))
+         (paths (plist-get p :paths))
+         (src-paths (--map (concat root "/" (car it)) paths))
+         (dst-paths (--map (concat root "/" (cdr it)) paths))
+         (libs (plist-get p :libs))
+         (expanded-libs (if libs libs (flycheck-java-expand-lib-paths root (plist-get p :lib-paths))))
+         (source-path (--reduce (concat acc ":" it) src-paths))
+         (class-path (--reduce (concat acc ":" it)
+                               (-flatten (list dst-paths (--map (concat root "/" it) expanded-libs))))))
     (append (list "-source"
-                  (cdr (assoc :source p))
+                  (plist-get p :source)
                   "-target"
-                  (cdr (assoc ':target p))
+                  (plist-get p :source)
                   "-classpath" class-path)
-            (if jsc/no-sourcepath nil `("-sourcepath" ,source-path)))))
+            (if flycheck-java-no-sourcepath nil `("-sourcepath" ,source-path)))))
 
-;; flycheck syntax checker
-(defun jsc/get-ecj-compiler-options ()
-  "pre-computes and stores project definition and checker command line in buffer variables"
-  (when (not (local-variable-p 'current-java-project))
-    (let* ((p (jsc/initialize default-directory))
-           (opts (jsc/make-compiler-options p)))
+;; checker options initialization
+(defun flycheck-java-get-cmd-options ()
+  "Pre-compute and store project definition and checker command line in buffer variables."
+  (when (not flycheck-java-project)
+    (let* ((project-def (flycheck-java-get-project-def default-directory))
+           (opts (flycheck-java-make-cmd-options project-def)))
       ;; project definition with pre-built command line options is kept in current-java-project buffer variable
-      (setq-local current-java-project p)
-      (setq-local compiler-cmd-options opts)))
-  compiler-cmd-options)
+      (setq-local flycheck-java-project project-def)
+      (setq-local flycheck-java-cmdopts opts)))
+  flycheck-java-cmdopts)
 
-(when (featurep 'flycheck-autoloads)
-  (flycheck-declare-checker java-syntax
-    "Check syntax of java code using ecj compiler"
-    :command `(,jsc/java
-               "-Xms128m"
-               "-Xmx128m"
-               "-jar" ,jsc/ecj-jar-path
-               "-d" "none"
-               "-Xemacs"
-               (eval (jsc/get-ecj-compiler-options))
-               source)
-    :error-patterns
-    (list (jsc/ecj-compile-pattern "warning" 'warning)
-          (jsc/ecj-compile-pattern "error" 'error))
-    :modes '(java-mode))
 
-  (add-to-list 'flycheck-checkers 'java-syntax))
+(flycheck-define-checker java
+  "Java syntax checker using ecj batch compiler."
+  :command ("java" 
+            (option "-jar" flycheck-java-ecj-jar-path)
+            "-d" "none" "-Xemacs" 
+            (eval (flycheck-java-get-cmd-options))
+            source)
+  :error-patterns 
+  ((warning line-start (file-name) ":" line ": warning:" 
+            (message (zero-or-more not-newline)) line-end)
+   (error line-start (file-name) ":" line ": error:" 
+          (message (zero-or-more not-newline)) line-end))
+  :modes java-mode)
 
-;; check using flymake
-(defun jsc/fm-init ()
-  "Initialize syntax checker command"
-  (let* ((cmd-options (jsc/get-ecj-compiler-options))
-         (temp-file (flymake-init-create-temp-buffer-copy
-                     'jsc/fm-create-temp-file))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
+(add-to-list 'flycheck-checkers 'java)
 
-    (list jsc/java
-          (append (list "-Xms128m"
-                        "-Xmx128m"
-                        "-jar" jsc/ecj-jar-path
-                        "-d" "none"
-                        "-Xemacs")
-                  cmd-options
-                  (list (expand-file-name local-file))))))
-
-(defun jsc/fm-cleanup ()
-  "Delete temp file and dirs."
-  (flymake-safe-delete-file flymake-temp-source-file-name)
-  (when flymake-temp-source-file-name
-    (flymake-safe-delete-directory (file-name-directory flymake-temp-source-file-name))))
-
-(defun jsc/fm-create-temp-file (file-name prefix)
-  "Create temporary directory file "
-  (file-truename (expand-file-name
-                  (file-name-nondirectory file-name)
-                  (expand-file-name  (int-to-string (abs (random))) (flymake-get-temp-dir)))))
-
-(when (featurep 'flymake)
-  (push '(".+\\.java$" jsc/fm-init jsc/fm-cleanup) flymake-allowed-file-name-masks))
-
-(provide 'java-syntax-checker)
+(provide 'flycheck-java)
+;;; flycheck-java ends here
